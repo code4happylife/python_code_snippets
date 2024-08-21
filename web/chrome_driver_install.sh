1227072521bac93773a1e626387246af919c8952
code4happylife <1559273720@qq.com>
2024-08-21 09:42:09 +0800

    Create chrome_driver_install.sh
    perform web automation test, and before that, you need to install chrome driver on your computer.

---
+++ web/chrome_driver_install.sh
@@ -0,0 +1,5 @@
+sudo cp chromedriver /usr/local/bin
+sudo chmod +x /usr/local/bin/chromedriver
+chromedriver --version
+#ChromeDriver 127.0.6533.119 (bdef6783a05f0b3f885591e7d2c7b2aec1a89dea-refs/branch-heads/6533@{#1999})
+#https://googlechromelabs.github.io/chrome-for-testing/
